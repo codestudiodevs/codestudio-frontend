8845fb9222c1bc371b2479817b5001c007616e80
codestudiodevs <143318942+codestudiodevs@users.noreply.github.com>
2023-08-27 20:09:56 +0400

    added build

---
+++ nuxt/components/index.js
@@ -0,0 +1,109 @@
+export const Activity = () => import('../..\\components\\Activity.vue' /* webpackChunkName: "components/activity" */).then(c => wrapFunctional(c.default || c))
+export const Allowance = () => import('../..\\components\\Allowance.vue' /* webpackChunkName: "components/allowance" */).then(c => wrapFunctional(c.default || c))
+export const AreaChart = () => import('../..\\components\\AreaChart.vue' /* webpackChunkName: "components/area-chart" */).then(c => wrapFunctional(c.default || c))
+export const AutoShift = () => import('../..\\components\\AutoShift.vue' /* webpackChunkName: "components/auto-shift" */).then(c => wrapFunctional(c.default || c))
+export const Calender = () => import('../..\\components\\Calender.vue' /* webpackChunkName: "components/calender" */).then(c => wrapFunctional(c.default || c))
+export const Cards = () => import('../..\\components\\Cards.vue' /* webpackChunkName: "components/cards" */).then(c => wrapFunctional(c.default || c))
+export const ComboChart = () => import('../..\\components\\ComboChart.vue' /* webpackChunkName: "components/combo-chart" */).then(c => wrapFunctional(c.default || c))
+export const Commision = () => import('../..\\components\\Commision.vue' /* webpackChunkName: "components/commision" */).then(c => wrapFunctional(c.default || c))
+export const ComonPreloader = () => import('../..\\components\\ComonPreloader.vue' /* webpackChunkName: "components/comon-preloader" */).then(c => wrapFunctional(c.default || c))
+export const CSV = () => import('../..\\components\\CSV.vue' /* webpackChunkName: "components/c-s-v" */).then(c => wrapFunctional(c.default || c))
+export const DailyLogs = () => import('../..\\components\\DailyLogs.vue' /* webpackChunkName: "components/daily-logs" */).then(c => wrapFunctional(c.default || c))
+export const Deduction = () => import('../..\\components\\Deduction.vue' /* webpackChunkName: "components/deduction" */).then(c => wrapFunctional(c.default || c))
+export const Document = () => import('../..\\components\\Document.vue' /* webpackChunkName: "components/document" */).then(c => wrapFunctional(c.default || c))
+export const Donut = () => import('../..\\components\\Donut.vue' /* webpackChunkName: "components/donut" */).then(c => wrapFunctional(c.default || c))
+export const Editor = () => import('../..\\components\\Editor.vue' /* webpackChunkName: "components/editor" */).then(c => wrapFunctional(c.default || c))
+export const Employee = () => import('../..\\components\\Employee.vue' /* webpackChunkName: "components/employee" */).then(c => wrapFunctional(c.default || c))
+export const EMPLOYEETOREPORT = () => import('../..\\components\\EMPLOYEETOREPORT.vue' /* webpackChunkName: "components/e-m-p-l-o-y-e-e-t-o-r-e-p-o-r-t" */).then(c => wrapFunctional(c.default || c))
+export const FacePreloader = () => import('../..\\components\\FacePreloader.vue' /* webpackChunkName: "components/face-preloader" */).then(c => wrapFunctional(c.default || c))
+export const FiloShift = () => import('../..\\components\\FiloShift.vue' /* webpackChunkName: "components/filo-shift" */).then(c => wrapFunctional(c.default || c))
+export const GenerateLog = () => import('../..\\components\\GenerateLog.vue' /* webpackChunkName: "components/generate-log" */).then(c => wrapFunctional(c.default || c))
+export const Logs = () => import('../..\\components\\Logs.vue' /* webpackChunkName: "components/logs" */).then(c => wrapFunctional(c.default || c))
+export const MultiInOutShift = () => import('../..\\components\\MultiInOutShift.vue' /* webpackChunkName: "components/multi-in-out-shift" */).then(c => wrapFunctional(c.default || c))
+export const NoAccess = () => import('../..\\components\\NoAccess.vue' /* webpackChunkName: "components/no-access" */).then(c => wrapFunctional(c.default || c))
+export const NoRecordFound = () => import('../..\\components\\NoRecordFound.vue' /* webpackChunkName: "components/no-record-found" */).then(c => wrapFunctional(c.default || c))
+export const OriginalLogs = () => import('../..\\components\\OriginalLogs.vue' /* webpackChunkName: "components/original-logs" */).then(c => wrapFunctional(c.default || c))
+export const OverNightShift = () => import('../..\\components\\OverNightShift.vue' /* webpackChunkName: "components/over-night-shift" */).then(c => wrapFunctional(c.default || c))
+export const Overtime = () => import('../..\\components\\Overtime.vue' /* webpackChunkName: "components/overtime" */).then(c => wrapFunctional(c.default || c))
+export const PIE = () => import('../..\\components\\PIE.vue' /* webpackChunkName: "components/p-i-e" */).then(c => wrapFunctional(c.default || c))
+export const PIE2 = () => import('../..\\components\\PIE2.vue' /* webpackChunkName: "components/p-i-e2" */).then(c => wrapFunctional(c.default || c))
+export const PiePreloader = () => import('../..\\components\\PiePreloader.vue' /* webpackChunkName: "components/pie-preloader" */).then(c => wrapFunctional(c.default || c))
+export const Preloader = () => import('../..\\components\\Preloader.vue' /* webpackChunkName: "components/preloader" */).then(c => wrapFunctional(c.default || c))
+export const RealTimeLogs = () => import('../..\\components\\RealTimeLogs.vue' /* webpackChunkName: "components/real-time-logs" */).then(c => wrapFunctional(c.default || c))
+export const RenderAttendance = () => import('../..\\components\\RenderAttendance.vue' /* webpackChunkName: "components/render-attendance" */).then(c => wrapFunctional(c.default || c))
+export const SingleShift = () => import('../..\\components\\SingleShift.vue' /* webpackChunkName: "components/single-shift" */).then(c => wrapFunctional(c.default || c))
+export const SplitShift = () => import('../..\\components\\SplitShift.vue' /* webpackChunkName: "components/split-shift" */).then(c => wrapFunctional(c.default || c))
+export const AttendanceReportGeneralAttendanceReport = () => import('../..\\components\\attendance_report\\GeneralAttendanceReport.vue' /* webpackChunkName: "components/attendance-report-general-attendance-report" */).then(c => wrapFunctional(c.default || c))
+export const AttendanceReportMimoAttendanceReport = () => import('../..\\components\\attendance_report\\MimoAttendanceReport.vue' /* webpackChunkName: "components/attendance-report-mimo-attendance-report" */).then(c => wrapFunctional(c.default || c))
+export const ChartsPIE = () => import('../..\\components\\charts\\PIE.vue' /* webpackChunkName: "components/charts-p-i-e" */).then(c => wrapFunctional(c.default || c))
+export const EmployeeBank = () => import('../..\\components\\employee\\Bank.vue' /* webpackChunkName: "components/employee-bank" */).then(c => wrapFunctional(c.default || c))
+export const EmployeeContact = () => import('../..\\components\\employee\\Contact.vue' /* webpackChunkName: "components/employee-contact" */).then(c => wrapFunctional(c.default || c))
+export const EmployeeCreateEmployee = () => import('../..\\components\\employee\\CreateEmployee.vue' /* webpackChunkName: "components/employee-create-employee" */).then(c => wrapFunctional(c.default || c))
+export const EmployeeDocument = () => import('../..\\components\\employee\\Document.vue' /* webpackChunkName: "components/employee-document" */).then(c => wrapFunctional(c.default || c))
+export const EmployeeEmirates = () => import('../..\\components\\employee\\Emirates.vue' /* webpackChunkName: "components/employee-emirates" */).then(c => wrapFunctional(c.default || c))
+export const EmployeeDetails = () => import('../..\\components\\employee\\EmployeeDetails.vue' /* webpackChunkName: "components/employee-details" */).then(c => wrapFunctional(c.default || c))
+export const EmployeeEmployeedevisesmap2 = () => import('../..\\components\\employee\\employeedevisesmap2.vue' /* webpackChunkName: "components/employee-employeedevisesmap2" */).then(c => wrapFunctional(c.default || c))
+export const EmployeeEdit = () => import('../..\\components\\employee\\EmployeeEdit.vue' /* webpackChunkName: "components/employee-edit" */).then(c => wrapFunctional(c.default || c))
+export const EmployeeKeyValueTable = () => import('../..\\components\\employee\\KeyValueTable.vue' /* webpackChunkName: "components/employee-key-value-table" */).then(c => wrapFunctional(c.default || c))
+export const EmployeeLogin = () => import('../..\\components\\employee\\Login.vue' /* webpackChunkName: "components/employee-login" */).then(c => wrapFunctional(c.default || c))
+export const EmployeeLogs = () => import('../..\\components\\employee\\Logs.vue' /* webpackChunkName: "components/employee-logs" */).then(c => wrapFunctional(c.default || c))
+export const EmployeePassport = () => import('../..\\components\\employee\\Passport.vue' /* webpackChunkName: "components/employee-passport" */).then(c => wrapFunctional(c.default || c))
+export const EmployeePayroll = () => import('../..\\components\\employee\\Payroll.vue' /* webpackChunkName: "components/employee-payroll" */).then(c => wrapFunctional(c.default || c))
+export const EmployeePersonal = () => import('../..\\components\\employee\\Personal.vue' /* webpackChunkName: "components/employee-personal" */).then(c => wrapFunctional(c.default || c))
+export const EmployeeQualification = () => import('../..\\components\\employee\\Qualification.vue' /* webpackChunkName: "components/employee-qualification" */).then(c => wrapFunctional(c.default || c))
+export const EmployeeSetting = () => import('../..\\components\\employee\\Setting.vue' /* webpackChunkName: "components/employee-setting" */).then(c => wrapFunctional(c.default || c))
+export const EmployeeVisa = () => import('../..\\components\\employee\\Visa.vue' /* webpackChunkName: "components/employee-visa" */).then(c => wrapFunctional(c.default || c))
+export const EmployeeWorkInfo = () => import('../..\\components\\employee\\WorkInfo.vue' /* webpackChunkName: "components/employee-work-info" */).then(c => wrapFunctional(c.default || c))
+export const HostList = () => import('../..\\components\\Host\\List.vue' /* webpackChunkName: "components/host-list" */).then(c => wrapFunctional(c.default || c))
+export const ScheduleEmployeeScheduledEmployees = () => import('../..\\components\\schedule_employee\\ScheduledEmployees.vue' /* webpackChunkName: "components/schedule-employee-scheduled-employees" */).then(c => wrapFunctional(c.default || c))
+export const ScheduleEmployeeUnScheduledEmployees = () => import('../..\\components\\schedule_employee\\UnScheduledEmployees.vue' /* webpackChunkName: "components/schedule-employee-un-scheduled-employees" */).then(c => wrapFunctional(c.default || c))
+export const SnippetsBack = () => import('../..\\components\\Snippets\\Back.vue' /* webpackChunkName: "components/snippets-back" */).then(c => wrapFunctional(c.default || c))
+export const SnippetsCamera = () => import('../..\\components\\Snippets\\Camera.vue' /* webpackChunkName: "components/snippets-camera" */).then(c => wrapFunctional(c.default || c))
+export const ThemeCardDesginer = () => import('../..\\components\\Theme\\CardDesginer.vue' /* webpackChunkName: "components/theme-card-desginer" */).then(c => wrapFunctional(c.default || c))
+export const VisitorList = () => import('../..\\components\\Visitor\\List.vue' /* webpackChunkName: "components/visitor-list" */).then(c => wrapFunctional(c.default || c))
+export const VisitorsReportVisitorReport = () => import('../..\\components\\visitors_report\\VisitorReport.vue' /* webpackChunkName: "components/visitors-report-visitor-report" */).then(c => wrapFunctional(c.default || c))
+export const WidgetsAnnouncement = () => import('../..\\components\\widgets\\Announcement.vue' /* webpackChunkName: "components/widgets-announcement" */).then(c => wrapFunctional(c.default || c))
+export const WidgetsAttendanceLog = () => import('../..\\components\\widgets\\AttendanceLog.vue' /* webpackChunkName: "components/widgets-attendance-log" */).then(c => wrapFunctional(c.default || c))
+export const WidgetsAttendancePIE = () => import('../..\\components\\widgets\\AttendancePIE.vue' /* webpackChunkName: "components/widgets-attendance-p-i-e" */).then(c => wrapFunctional(c.default || c))
+export const WidgetsDashboardCardComponent = () => import('../..\\components\\widgets\\DashboardCardComponent.vue' /* webpackChunkName: "components/widgets-dashboard-card-component" */).then(c => wrapFunctional(c.default || c))
+export const WidgetsDashboardComponent = () => import('../..\\components\\widgets\\DashboardComponent.vue' /* webpackChunkName: "components/widgets-dashboard-component" */).then(c => wrapFunctional(c.default || c))
+export const WidgetsDepartmentMappingCreate = () => import('../..\\components\\widgets\\DepartmentMappingCreate.vue' /* webpackChunkName: "components/widgets-department-mapping-create" */).then(c => wrapFunctional(c.default || c))
+export const WidgetsDevice = () => import('../..\\components\\widgets\\Device.vue' /* webpackChunkName: "components/widgets-device" */).then(c => wrapFunctional(c.default || c))
+export const WidgetsDevicePIE = () => import('../..\\components\\widgets\\DevicePIE.vue' /* webpackChunkName: "components/widgets-device-p-i-e" */).then(c => wrapFunctional(c.default || c))
+export const WidgetsRealTimeLog = () => import('../..\\components\\widgets\\RealTimeLog.vue' /* webpackChunkName: "components/widgets-real-time-log" */).then(c => wrapFunctional(c.default || c))
+export const WidgetsReportNotification = () => import('../..\\components\\widgets\\ReportNotification.vue' /* webpackChunkName: "components/widgets-report-notification" */).then(c => wrapFunctional(c.default || c))
+export const ZoneList = () => import('../..\\components\\Zone\\List.vue' /* webpackChunkName: "components/zone-list" */).then(c => wrapFunctional(c.default || c))
+export const SnippetsFiltersDateRangePicker = () => import('../..\\components\\Snippets\\Filters\\DateRangePicker.vue' /* webpackChunkName: "components/snippets-filters-date-range-picker" */).then(c => wrapFunctional(c.default || c))
+export const SnippetsFiltersDropDown = () => import('../..\\components\\Snippets\\Filters\\DropDown.vue' /* webpackChunkName: "components/snippets-filters-drop-down" */).then(c => wrapFunctional(c.default || c))
+export const SnippetsFiltersTextField = () => import('../..\\components\\Snippets\\Filters\\TextField.vue' /* webpackChunkName: "components/snippets-filters-text-field" */).then(c => wrapFunctional(c.default || c))
+
+// nuxt/nuxt.js#8607
+function wrapFunctional(options) {
+  if (!options || !options.functional) {
+    return options
+  }
+
+  const propKeys = Array.isArray(options.props) ? options.props : Object.keys(options.props || {})
+
+  return {
+    render(h) {
+      const attrs = {}
+      const props = {}
+
+      for (const key in this.$attrs) {
+        if (propKeys.includes(key)) {
+          props[key] = this.$attrs[key]
+        } else {
+          attrs[key] = this.$attrs[key]
+        }
+      }
+
+      return h(options, {
+        on: this.$listeners,
+        attrs,
+        props,
+        scopedSlots: this.$scopedSlots,
+      }, this.$slots.default)
+    }
+  }
+}
