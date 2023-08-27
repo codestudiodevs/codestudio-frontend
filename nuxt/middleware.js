3d101bc57eba9adf61b682b0cb16da481f1fef5a
codestudiodevs <143318942+codestudiodevs@users.noreply.github.com>
2023-08-27 20:52:06 +0400

    added build

--- nuxt/middleware.js
+++
@@ -1,9 +0,0 @@
-const middleware = {}
-
-middleware['userType'] = require('..\\middleware\\userType.js')
-middleware['userType'] = middleware['userType'].default || middleware['userType']
-
-middleware['verify'] = require('..\\middleware\\verify.js')
-middleware['verify'] = middleware['verify'].default || middleware['verify']
-
-export default middleware
