1f9d8fedc42f4bdcf8a1a47d6df507e7cebb94fb
codestudiodevs <143318942+codestudiodevs@users.noreply.github.com>
2023-08-27 21:12:33 +0400

    remove extras

--- .nuxt-prod/dist1/client/a1d5fd0.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[99],{1230:function(n,t,e){"use strict";e.r(t);var o={created:function(){this.$router.push("/dashboard2")}},r=e(61),component=Object(r.a)(o,(function(){return(0,this._self._c)("div")}),[],!1,null,null,null);t.default=component.exports}}]);
