d4e4e3e4b99be3bebcb0ea3fd4aac35d3b5eaade
codestudiodevs <143318942+codestudiodevs@users.noreply.github.com>
2023-08-27 20:15:43 +0400

    added build

---
+++ .nuxt1/vuetify/options.js
@@ -0,0 +1,1 @@
+export default {"theme":{"dark":false,"themes":{"light":{"primary":"#5fafa3","accent":"#d8363a","secondary":"#242424","background":"#34444c","info":"#26a69a","warning":"#ffc107","error":"#dd2c00","success":"#00e676","main_bg":"#ECF0F4"}}}}
